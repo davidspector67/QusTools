320e32c6cfc98aced6b9ca8f6b5907a69e12bd4e
David Spector <davidspector9@gmail.com>
2023-09-01 18:13:11 -0700

    Trying fusion app style for windows

--- main.py
+++ main.py
@@ -58,5 +58,6 @@ class QusGui(Ui_qusPage, QWidget):
 if __name__ == "__main__":
     welcomeApp = QApplication(sys.argv)
     welcomeUI = QusGui()
+    welcomeApp.setStyle('fusion')
     welcomeUI.show()
     sys.exit(welcomeApp.exec())
