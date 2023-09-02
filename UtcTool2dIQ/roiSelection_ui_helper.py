7e4657597163f0f235a4013225a235e4d3eed9c9
David Spector <davidspector9@gmail.com>
2023-09-01 19:26:00 -0700

    Windows style changes

--- UtcTool2dIQ/roiSelection_ui_helper.py
+++ UtcTool2dIQ/roiSelection_ui_helper.py
@@ -17,11 +17,79 @@ import scipy.interpolate as interpolate
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtGui import QImage
 
+import platform
+system = platform.sysetm()
+
 
 class RoiSelectionGUI(QWidget, Ui_constructRoi):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        if system == 'Windows':
+            self.roiSidebarLabel.setStyleSheet("""QLabel { 
+                font-size: 18px; 
+                color: rgb(255, 255, 255); 
+                background-color: rgba(255, 255, 255, 0); 
+                border: 0px; 
+                font-weight: bold; 
+            }""")
+            self.imageSelectionLabelSidebar.setStyleSheet("""QLabel {
+                font-size: 18px;
+                color: rgb(255, 255, 255);
+                background-color: rgba(255, 255, 255, 0);
+                border: 0px;
+                font-weight: bold;
+            }""")
+            self.imageLabel.setStyleSheet("""QLabel {
+                font-size: 13px;
+                color: rgb(255, 255, 255);
+                background-color: rgba(255, 255, 255, 0);
+                border: 0px;
+                font-weight: bold;
+            }""")
+            self.phantomLabel.setStyleSheet("""QLabel {
+                font-size: 13px;
+                color: rgb(255, 255, 255);
+                background-color: rgba(255, 255, 255, 0);
+                border: 0px;
+                font-weight: bold;
+            }""")
+            self.imageFilenameDisplay.setStyleSheet("""QLabel {
+                font-size: 11px;
+                color: rgb(255, 255, 255);
+                background-color: rgba(255, 255, 255, 0);
+                border: 0px;
+            }""")
+            self.phantomFilenameDisplay.setStyleSheet("""QLabel {
+                font-size: 11px;
+                color: rgb(255, 255, 255);
+                background-color: rgba(255, 255, 255, 0);
+                border: 0px;
+            }""")
+            self.analysisParamsLabel.setStyleSheet("""QLabel {
+                font-size: 18px;
+                color: rgb(255, 255, 255);
+                background-color: rgba(255, 255, 255, 0);
+                border: 0px;
+                font-weight:bold;
+            }""")
+            self.rfAnalysisLabel.setStyleSheet("""QLabel {
+                font-size: 18px;
+                color: rgb(255, 255, 255);
+                background-color: rgba(255, 255, 255, 0);
+                border: 0px;
+                font-weight:bold;
+            }""")
+            self.exportResultsLabel.setStyleSheet("""QLabel {
+                font-size: 18px;
+                color: rgb(255, 255, 255);
+                background-color: rgba(255, 255, 255, 0);
+                border: 0px;
+                font-weight:bold;
+            }""")
+
+            
         self.imagePathInput.setHidden(True)
         self.phantomPathInput.setHidden(True)
         self.drawRoiButton.setHidden(True)
--- UtcTool2dIQ/selectImage_ui_helper.py
+++ UtcTool2dIQ/selectImage_ui_helper.py
@@ -25,23 +25,66 @@ class SelectImageGUI_UtcTool2dIQ(Ui_selectImage, QWidget):
 
         if system == 'Windows':
             self.roiSidebarLabel.setStyleSheet("""QLabel { 
-                font-size: 21px; 
+                font-size: 18px; 
                 color: rgb(255, 255, 255); 
                 background-color: rgba(255, 255, 255, 0); 
                 border: 0px; 
                 font-weight: bold; 
             }""")
-            self.imageSelectionLabelSidebar.font().setPointSize(18)
-            self.imageLabel.font().setPointSize(13)
-            self.phantomLabel.font().setPointSize(13)
-            self.imageFilenameDisplay.font().setPointSize(11)
-            self.phantomFilenameDisplay.font().setPointSize(11)
-            self.analysisParamsLabel.font().setPointSize(18)
-            self.rfAnalysisLabel.font().setPointSize(18)
-            self.exportResultsLabel.font().setPointSize(18)
-
-            
-
+            self.imageSelectionLabelSidebar.setStyleSheet("""QLabel {
+                font-size: 18px;
+                color: rgb(255, 255, 255);
+                background-color: rgba(255, 255, 255, 0);
+                border: 0px;
+                font-weight: bold;
+            }""")
+            self.imageLabel.setStyleSheet("""QLabel {
+                font-size: 13px;
+                color: rgb(255, 255, 255);
+                background-color: rgba(255, 255, 255, 0);
+                border: 0px;
+                font-weight: bold;
+            }""")
+            self.phantomLabel.setStyleSheet("""QLabel {
+                font-size: 13px;
+                color: rgb(255, 255, 255);
+                background-color: rgba(255, 255, 255, 0);
+                border: 0px;
+                font-weight: bold;
+            }""")
+            self.imageFilenameDisplay.setStyleSheet("""QLabel {
+                font-size: 11px;
+                color: rgb(255, 255, 255);
+                background-color: rgba(255, 255, 255, 0);
+                border: 0px;
+            }""")
+            self.phantomFilenameDisplay.setStyleSheet("""QLabel {
+                font-size: 11px;
+                color: rgb(255, 255, 255);
+                background-color: rgba(255, 255, 255, 0);
+                border: 0px;
+            }""")
+            self.analysisParamsLabel.setStyleSheet("""QLabel {
+                font-size: 18px;
+                color: rgb(255, 255, 255);
+                background-color: rgba(255, 255, 255, 0);
+                border: 0px;
+                font-weight:bold;
+            }""")
+            self.rfAnalysisLabel.setStyleSheet("""QLabel {
+                font-size: 18px;
+                color: rgb(255, 255, 255);
+                background-color: rgba(255, 255, 255, 0);
+                border: 0px;
+                font-weight:bold;
+            }""")
+            self.exportResultsLabel.setStyleSheet("""QLabel {
+                font-size: 18px;
+                color: rgb(255, 255, 255);
+                background-color: rgba(255, 255, 255, 0);
+                border: 0px;
+                font-weight:bold;
+            }""")
 
         self.chooseImageFileButton.setHidden(True)
         self.choosePhantomFileButton.setHidden(True)
